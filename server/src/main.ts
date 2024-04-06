56b7ecd6aec346ea798747a5fc1fcd6fb55579fe
CEPHAL0 <xaradxarma@gmail.com>
2024-04-06 12:56:40 +0545

    Added Comment

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger (OpenAPI) Configuration
   const config = new DocumentBuilder()
     .setTitle('Survey Application')
     .setDescription('Survey API application')
